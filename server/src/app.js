d6b93a9585f5e1cfe60502d4bb001725e3263c50
Cao Phong <leecaophong@gmail.com>
2024-12-10 01:06:46 +0700

    update body parser

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import homeRouter from './routers/homeRouter.js';
 import storeDetailsRouter from './routers/storeDetailsRouter.js';
 import authRouter from './routers/authRouter.js';
@@ -12,13 +13,18 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 // Đăng ký các router không yêu cầu xác thực
 app.use('/auth', authRouter);  // Dành cho đăng nhập, không cần authenticate
 
 // Đăng ký các router yêu cầu xác thực
 app.use('/api', authMiddleware, homeRouter);  // Thêm authMiddleware vào để bảo vệ route
-app.use('/api/store', authMiddleware, storeDetailsRouter);
+app.use('/api/store',authMiddleware, storeDetailsRouter);
 app.use('/api/blacklist', authMiddleware, blacklistRouter)  // Cũng có thể bảo vệ các route khác
 
 // Xuất app
