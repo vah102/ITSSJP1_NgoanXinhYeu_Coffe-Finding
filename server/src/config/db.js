0902ec839acb8bb640057cfb785aa964ee5e2913
lenhung1107 <tink60chuyenbienhoa@gmail.com>
2024-12-27 21:51:31 +0700

    i18nex fix

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("itss", "root", "Tink60nhungle", {
+const sequelize = new Sequelize("itss", "root", "123456", {
     host: "localhost",
     dialect: "mysql",
     logging: false, // Tắt log nếu không cần
